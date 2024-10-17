db466d1047e8ca989816502265ce38d0ddaaf41f
Кирилл Ляпунов <kirilllapunov@MacBook-Air-Kirill.local>
2024-10-17 18:21:34 +0300

    updated Engine.php

--- src/Engine.php
+++ src/Engine.php
@@ -10,74 +10,102 @@ function gameEngine($gameType)
     for ($i = 0; $i < 3; $i++) {
         switch ($gameType) {
             case 'EvenOdd':
-                $randNum = rand(1, 100);
-                $userAnswer = strtolower(prompt("Question: {$randNum} \nYour answer"));
-    
-                $rightAnswer = $randNum % 2 == 0 ? 'yes' : 'no';
+                $answers = playEvenOdd();
                 break;
             case 'Calc':
-                $a = rand(0, 100);
-                $b = rand(0, 100);
-                $operation = [' + ', ' - ', ' * '];
-
-                $operand = $operation[rand(0, count($operation) - 1)];
-                $userAnswer = prompt("Question: {$a} {$operand} {$b} \nYour answer");
-
-                switch ($operand) {
-                    case ' + ':
-                        $rightAnswer = $a + $b;
-                        break;
-                    case ' - ':
-                        $rightAnswer = $a - $b;
-                        break;
-                    case ' * ':
-                        $rightAnswer = $a * $b;
-                        break;
-                }
+                $answers = playCalc();
                 break;
             case 'Gcd':
-                $a = rand(1, 100);
-                $b = rand(1, 100);
-                $userAnswer = prompt("Question: {$a} {$b} \nYour answer");
-                $rightAnswer = gmp_gcd($a, $b);
+                $answers = playGcd();
                 break;
             case 'Progression':
-                unset($progression);
-                $progressionLength = rand(5,10);
-                $progression[0] = rand(0, 100);
-                $step = rand(1, 5);
-
-                for ($j = 1; $j < $progressionLength; $j++) {
-                    $progression[$j] = $progression[$j - 1] + $step;
-                }
-                
-                $hidedPosition = rand(1, $progressionLength);
-                $rightAnswer = $progression[$hidedPosition - 1];
-                $progression[$hidedPosition - 1] = '..';
-                $stringProgression = implode(' ',$progression);
-                $userAnswer = prompt("Question: {$stringProgression} \nYour answer");
+                $answers = playProgression();
                 break;
             case 'Prime':
-                $num = rand(1, 150);
-                $rightAnswer = '';
-                if (gmp_prob_prime($num) !== 0) {
-                    $rightAnswer = 'yes';
-                }
-                else {
-                    $rightAnswer = 'no';
-                }
-                $userAnswer = prompt("Question: {$num} \nYour answer");
+                $answers = playPrime();
                 break;
         }
            
-        if ($userAnswer == $rightAnswer) {
+        if ($answers[0] == $answers[1]) {
             line('Correct!');
         } else {
-            $finalmessage = "'{$userAnswer}' is wrong answer ;(. Correct answer was '{$rightAnswer}'. \nLet's try again, %s!";
+            $finalmessage = "'{$answers[0]}' is wrong answer ;(. Correct answer was '{$answers[1]}'. \nLet's try again, %s!";
             return $finalmessage;
         }
     }
     $finalmessage = "Congratulations, %s!";
 
     return $finalmessage;
+}
+
+function playEvenOdd() {
+    $randNum = rand(1, 100);
+    $userAnswer = strtolower(prompt("Question: {$randNum} \nYour answer"));
+
+    $rightAnswer = $randNum % 2 == 0 ? 'yes' : 'no';
+
+    $answers = [$userAnswer, $rightAnswer];
+    return $answers;
+}
+function playCalc() {
+    $a = rand(0, 100);
+    $b = rand(0, 100);
+    $operation = [' + ', ' - ', ' * '];
+
+    $operand = $operation[rand(0, count($operation) - 1)];
+    $userAnswer = prompt("Question: {$a} {$operand} {$b} \nYour answer");
+
+    switch ($operand) {
+        case ' + ':
+            $rightAnswer = $a + $b;
+            break;
+        case ' - ':
+            $rightAnswer = $a - $b;
+            break;
+        case ' * ':
+            $rightAnswer = $a * $b;
+            break;
+    }
+    $answers = [$userAnswer, $rightAnswer];
+    return $answers;
+}
+function playGcd() {
+    $a = rand(1, 100);
+    $b = rand(1, 100);
+    $userAnswer = prompt("Question: {$a} {$b} \nYour answer");
+    $rightAnswer = gmp_gcd($a, $b);
+
+    $answers = [];
+    return $answers = [$userAnswer, $rightAnswer];
+}
+function playProgression() {
+    unset($progression);
+    $progressionLength = rand(5,10);
+    $progression[0] = rand(0, 100);
+    $step = rand(1, 5);
+
+    for ($j = 1; $j < $progressionLength; $j++) {
+        $progression[$j] = $progression[$j - 1] + $step;
+    }
+    
+    $hidedPosition = rand(1, $progressionLength);
+    $rightAnswer = $progression[$hidedPosition - 1];
+    $progression[$hidedPosition - 1] = '..';
+    $stringProgression = implode(' ',$progression);
+    $userAnswer = prompt("Question: {$stringProgression} \nYour answer");
+    $answers = [$userAnswer, $rightAnswer];
+    return $answers;
+}
+function playPrime() {
+    $num = rand(1, 150);
+    $rightAnswer = '';
+    if (gmp_prob_prime($num) !== 0) {
+        $rightAnswer = 'yes';
+    }
+    else {
+        $rightAnswer = 'no';
+    }
+    $userAnswer = prompt("Question: {$num} \nYour answer");
+    $answers = [$userAnswer, $rightAnswer];
+    return $answers;
 }
